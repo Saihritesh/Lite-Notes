76475e1c9dae626536330051fad26f47341db619
Saihritesh <saihritesh2004@gmail.com>
2026-01-16 10:28:40 +0530

    Added Static UI for SignUp Page

--- lite-notes/app/signup/page.tsx
+++ lite-notes/app/signup/page.tsx
@@ -1,5 +1,12 @@
 export default function SignUpPage() { 
     return (
-        <div>This is the Sign Up Page</div>
+        <div>
+            <h1>This is the SignUp Page</h1>
+            <input type="email" placeholder = "Enter your email"/> 
+            <input type="text" placeholder = "Enter a valid Username"/>
+            <input type="password" placeholder = "Enter Password"/>
+            <input type="password" placeholder ="Confirm Password"/>
+            <button>SignUp</button>
+        </div>
     )
 }
