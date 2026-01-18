9350697579ef69a10b3118bfb105e33bd3adc1f8
Saihritesh <saihritesh2004@gmail.com>
2026-01-18 12:31:09 +0530

    Added MongoDB connectivity

--- lite-notes/app/signup/page.tsx
+++ lite-notes/app/signup/page.tsx
@@ -1,12 +1,20 @@
+import { sign } from "crypto"
+
 export default function SignUpPage() { 
     return (
-        <div>
-            <h1>This is the SignUp Page</h1>
-            <input type="email" placeholder = "Enter your email"/> 
-            <input type="text" placeholder = "Enter a valid Username"/>
-            <input type="password" placeholder = "Enter Password"/>
-            <input type="password" placeholder ="Confirm Password"/>
-            <button>SignUp</button>
+        
+        <div className ="items-center justify-center h-screen">
+            <div className ="flex flex-col gap-5 max-w-md mx-auto mt-10 justify-center h-screen">
+                   <label >Enter your email</label>
+                    <input type="email" placeholder = "Enter your email"/> 
+                    <label>Enter a valid Username</label>
+                    <input type="text" placeholder = "Enter a valid Username"/>
+                    <label>Enter a Password</label>
+                    <input type="password" placeholder="..."/>
+                    <label>Confirm Password</label>
+                    <input type="password" placeholder="..."/>
+                    <input className = "bg-blue-500 text-white" type="submit" value="SignUp"/>
+            </div>
         </div>
     )
 }
